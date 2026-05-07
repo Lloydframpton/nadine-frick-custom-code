3c8f3d4039c02c21ac0e08d57227d7685fcc6541
Lloydframpton <llmframpton@gmail.com>
2026-05-07 21:02:50 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
 <script>
   // ---- Carousel Code
-document.addEventListener('DOMContentLoaded', function() {
-  const track = document.querySelector('.carousel-track');
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    const track = document.querySelector('.carousel-track');
 
-  const items = Array.from(track.children);
-  items.forEach(item => {
-    const clone = item.cloneNode(true);
-    clone.setAttribute('aria-hidden', 'true');
-    track.appendChild(clone);
-  });
+    const items = Array.from(track.children);
+    items.forEach(item => {
+      const clone = item.cloneNode(true);
+      clone.setAttribute('aria-hidden', 'true');
+      track.appendChild(clone);
+    });
 
-  track.addEventListener('mouseenter', () => track.classList.add('paused'));
-  track.addEventListener('mouseleave', () => track.classList.remove('paused'));
+    track.addEventListener('mouseenter', () => track.classList.add('paused'));
+    track.addEventListener('mouseleave', () => track.classList.remove('paused'));
+  }, 500);
 });
 </script>
